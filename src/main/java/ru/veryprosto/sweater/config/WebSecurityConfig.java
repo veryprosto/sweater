b7d308651ecddae55ff9a2e569fd9e15d3ec0d51
Mikhail Grigorev <grigproject@ya.ru>
2021-04-15 08:14:18 +0300

    Add Admin role and check rights by `hasAuthority`

--- src/main/java/ru/veryprosto/sweater/config/WebSecurityConfig.java
+++ src/main/java/ru/veryprosto/sweater/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package ru.veryprosto.sweater.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,9 +12,9 @@ import ru.veryprosto.sweater.service.UserService;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true) //аннотация для того чтобы работала аннотация @PreAuthorize("hasAuthority('ADMIN')") в контроллере
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    /*@Autowired
-    private DataSource dataSource; *///датасорс генерируется спрингом и мы его можем легко получить
+
 
     @Autowired
     private UserService userService;
